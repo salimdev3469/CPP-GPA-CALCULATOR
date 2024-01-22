62a10028983ca9473a59c3be27f1080f7ee63a45
salimdev3469 <157348323+salimdev3469@users.noreply.github.com>
2024-01-22 14:37:41 +0300

    GPA CALCULATOR CPP
    This program calculates the user's GPA by taking an unlimited number of courses and an unlimited number of grades from the user.

---
+++ GPA_CALCULATOR/gpa_calc.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+using namespace std;
+
+void ortbul(float nots[], float notlar) {
+    cout << "WHAT IS PASS GRADE FOR THIS COURSE?" << endl;
+    float gecmenot;
+    cin >> gecmenot;
+    float genelort = 0;
+    for (int i = 0; i < notlar; i++) {
+        genelort = genelort + nots[i];
+    }
+    float genel = genelort / notlar;
+    cout << "GPA: " << genel << endl;
+    if (genel >= gecmenot) {
+        cout << "PASS." << endl;
+    }
+    else {
+        cout << "FAIL" << endl;
+    }
+}
+
+void getname(string name) {
+    cout << "Grades for " << name << " course"<<endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "Turkish");
+    cout << "***WELCOME TO GPA CALCULATOR***" << endl;
+    cout << "Input the course number: ";
+    int dersSayisi;
+    cin >> dersSayisi;
+
+    string* dersAdlari = new string[dersSayisi];
+    vector<string> secilenDersAdlari; 
+
+    for (int i = 0; i < dersSayisi; ++i) {
+        cout << i + 1 << "-" << "Course Name: ";
+        cin >> dersAdlari[i];
+    }
+
+    cout << "**COURSE NAMES**\n";
+    for (int i = 0; i < dersSayisi; ++i) {
+        cout << i + 1 << "-" << dersAdlari[i] << endl;
+    }
+
+    srand(static_cast<unsigned int>(time(0)));
+
+    for (int i = 0; i < dersSayisi; i++) {
+        int randomDersIndex;
+        do {
+            randomDersIndex = rand() % dersSayisi;
+        } while (find(secilenDersAdlari.begin(), secilenDersAdlari.end(), dersAdlari[randomDersIndex]) != secilenDersAdlari.end());
+
+        secilenDersAdlari.push_back(dersAdlari[randomDersIndex]);
+        getname(dersAdlari[randomDersIndex]);
+
+        cout << "How many different grade you will input ? (QUIZ, MIDTERM, FINAL, ATTENDANCE..) : ";
+        float notlar;
+        cin >> notlar;
+
+        float* notsayi = new float[notlar];
+
+        cout << "Input the grades: " << endl;
+        for (int j = 0; j < notlar; j++) {
+            cout << j + 1 << ". Grade:  ";
+            cin >> notsayi[j];
+        }
+        ortbul(notsayi, notlar);
+        delete[] notsayi;
+    }
+    delete[] dersAdlari;
+    return 0;
+}
